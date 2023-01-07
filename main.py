828f7e4e9ceb1d09f0be0c4617d6579987b23e8d
ABHISHEK GUPTA <85806664+1abhi6@users.noreply.github.com>
2023-01-07 21:36:37 +0530

    Changes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello")
+from geopy.geocoders import ArcGIS
+import folium
+
+obj = ArcGIS()
+locToBeSearched = input("Input the location to be searched: ")
+s = obj.geocode(locToBeSearched)
+map = folium.Map(location=[s.latitude, s.longitude], zoom_start=5)
+map.add_child(folium.Marker(location=[s.latitude, s.longitude],
+              popup=locToBeSearched.capitalize(), icon=folium.Icon(color="blue")))
+map.save('index.html')
